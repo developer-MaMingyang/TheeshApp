e03758d9acf7fc5d1f663e9fbd80bc706db52c7c
ma <poet0@vip.qq.com>
2019-02-14 14:56:34 +0800

    顶部加高度，Header需调整

--- src/views/Study/Container/styles.js
+++ src/views/Study/Container/styles.js
@@ -9,7 +9,7 @@ import { scaleSize } from '../../../utils/screen';
 const styles = StyleSheet.create({
     // 课程大类
     majorContainer: {
-        paddingTop: scaleSize(65),
+        paddingTop: scaleSize(100),
         marginBottom: scaleSize(56),
     },
     majorCover: {
