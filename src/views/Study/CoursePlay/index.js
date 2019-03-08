cd6ed9f86d5da28f152aec94a0bb03b06810afd0
poet-CN <971934542@qq.com>
2019-03-08 15:56:23 +0800

    暂存方案一

--- src/views/Study/CoursePlay/index.js
+++ src/views/Study/CoursePlay/index.js
@@ -6,17 +6,22 @@
 // TODO 待补充
 
 import React, { Component } from 'react';
-import { View, Text, FlatList, TouchableWithoutFeedback, WebView } from 'react-native';
+import { View, Text, FlatList, TouchableWithoutFeedback, WebView, ActivityIndicator, StatusBar } from 'react-native';
 import { Container, Content } from 'native-base';
 import { inject, observer } from 'mobx-react';
 import { Header } from '../../../components/HeaderBar';
-import { deviceWidth } from '../../../utils/screen';
+import { deviceHeight, deviceWidth } from '../../../utils/screen';
 import publicStyles from '../../../styles/public';
 import styles from './styles';
+import { fz } from '../../../styles/size';
 
 @inject(({ CoursePlayStore }) => ({ CoursePlayStore }))
 @observer
 class CoursePlay extends Component {
+    state = {
+        isFullScreen: false,
+    };
+
     componentDidMount() {
         const { CoursePlayStore: { initAuth }, navigation: { state: { params: { videoId } } } } = this.props;
         initAuth(videoId);
@@ -27,22 +32,60 @@ class CoursePlay extends Component {
         reset();
     }
 
+    handleMessage = (event) => {
+        console.log(event.nativeEvent.data);
+        switch (event.nativeEvent.data) {
+        case 'play':
+            this.setState({
+                isFullScreen: false,
+            });
+            // this.refs.webView.postMessage({
+            //     width: deviceWidth,
+            // });
+            break;
+        case 'pause':
+            this.setState({
+                isFullScreen: true,
+            });
+            StatusBar.setHidden(true);
+            // this.refs.webView.postMessage({
+            //     width: deviceHeight,
+            //     height: deviceWidth,
+            // });
+            break;
+        default:
+        }
+    };
+
     render() {
         const { CoursePlayStore: { playAuth }, navigation: { state: { params: { lessonName, coverUrl, videoId } } } } = this.props;
-        console.log(playAuth);
-        const source = require('../../../components/Player/index.html');
-        console.log(`http://192.168.131.2/video.html?playAuth=${playAuth}&coverUrl=${coverUrl}&videoId=${videoId}&deviceWidth=${deviceWidth}`);
+        console.log(deviceWidth);
+        console.log(deviceWidth / 48 * 27);
+        console.log(`http://192.168.131.2/video.html?playAuth=${playAuth}&coverUrl=${coverUrl}&videoId=${videoId}&deviceWidth=${deviceHeight}&deviceHeight=${deviceWidth}`);
         return (
             <Container>
-                <Header title={lessonName}/>
-                {playAuth ? (
-                    <WebView
-                        source={{ uri: `http://192.168.131.2/video.html?playAuth=${playAuth}&coverUrl=${coverUrl}&videoId=${videoId}` }}
-                        style={{ width: '100%', height: 1000 }}
-                    />
-                ) : (
-                    <View/>
-                )}
+
+                {!this.state.isFullScreen && (<Header title={lessonName} />)}
+                <Content>
+                    {playAuth ? (
+                        <WebView
+                            ref={webView => this.webView = webView}
+                            source={{ uri: `http://192.168.131.2/video.html?playAuth=${playAuth}&coverUrl=${coverUrl}&videoId=${videoId}&deviceWidth=${deviceWidth}` }}
+                            style={this.state.isFullScreen ? { width: deviceHeight, height: deviceWidth } : styles.videoWrapper}
+                            onMessage={this.handleMessage}
+                        />
+                    ) : (
+                        <View style={[publicStyles.aliC, publicStyles.jcC, styles.videoWrapper]}>
+                            <ActivityIndicator />
+                            <Text style={fz(25)}>加载中。。。</Text>
+                        </View>
+                    )}
+                    <Text style={fz(30)}>aaaaa</Text>
+                    <Text style={fz(30)}>aaaaa</Text>
+                    <Text style={fz(30)}>aaaaa</Text>
+                    <Text style={fz(30)}>aaaaa</Text>
+                    <Text style={fz(30)}>aaaaa</Text>
+                </Content>
             </Container>
         );
     }
--- src/views/Study/CoursePlay/styles.js
+++ src/views/Study/CoursePlay/styles.js
@@ -4,9 +4,13 @@
 */
 
 import { StyleSheet } from 'react-native';
+import { deviceWidth } from '../../../utils/screen';
 
 const styles = StyleSheet.create({
-
+    videoWrapper: {
+        width: deviceWidth,
+        height: deviceWidth / 48 * 27,
+    }
 });
 
 export default styles;
